34759ece8fa1847b78418df9354c510ea382da3c
Vitor <vitorbrz@gmail.com>
2015-04-23 21:44:47 -0300

    Imprime as 3 tabelas

--- ru.py
+++ ru.py
@@ -1,14 +1,26 @@
+import itertools as it
+
 from lxml import html
+
 
 url = "http://legiao.prefeitura.unicamp.br/cardapio.php"
 
 tree = html.parse(url)
 tabs = tree.xpath('//table[@class = "fundo_cardapio"]')
 
-almoco = tabs[0]
-for x in almoco.xpath(".//td/text()")[:-4]:
-    if not x.isspace():
-        print(x.lstrip())
+listas = []
+for periodo in tabs:
+    listas.append([])
+    l = listas[-1]
+    for x in periodo.xpath(".//td[not(strong='Observações:')]/text()"):
+        if not x.isspace():
+            l.append(x.lstrip())
+
+linhas = it.zip_longest(*listas, fillvalue='')
+fills_v = [max(map(len, x)) + 3 for x in listas]
+str_formato = "{0[0]:{0[1]}} {1[0]:{1[1]}} {2[0]:{2[1]}}"
+tabela = '\n'.join(str_formato.format(*zip(x, fills_v)) for x in linhas)
+print(tabela)
     
   
 
