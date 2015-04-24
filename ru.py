8fb95918e8934f4feb324cc1aaae8a4a1e0b72ba
Vitor <vitorbrz@gmail.com>
2015-04-23 22:14:49 -0300

    Mudei o loop

--- ru.py
+++ ru.py
@@ -8,19 +8,13 @@ url = "http://legiao.prefeitura.unicamp.br/cardapio.php"
 tree = html.parse(url)
 tabs = tree.xpath('//table[@class = "fundo_cardapio"]')
 
-listas = []
-for periodo in tabs:
-    listas.append([])
-    l = listas[-1]
-    for x in periodo.xpath(".//td[not(strong='Observações:')]/text()"):
-        if not x.isspace():
-            l.append(x.lstrip())
-
+listas = [[x.lstrip() for x
+            in periodo.xpath(".//td[not(strong='Observações:')]/text()")
+            if not x.isspace()]
+          for periodo in tabs]
+    
 linhas = it.zip_longest(*listas, fillvalue='')
 fills_v = [max(map(len, x)) + 3 for x in listas]
 str_formato = "{0[0]:{0[1]}} {1[0]:{1[1]}} {2[0]:{2[1]}}"
 tabela = '\n'.join(str_formato.format(*zip(x, fills_v)) for x in linhas)
 print(tabela)
-    
-  
-
